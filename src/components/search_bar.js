74b61467532f0db5868789a6259bfc202c91e207
Varun Chopra <vnchopra@gmail.com>
2018-07-18 18:13:15 -0700

    Added the youtube search dependency. Created a functional component (the search bar).

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//This is a functional component
+const SearchBar = () => {
+  return <input/>;
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = '';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create a new component that puts together HTML.
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+//Place that into the DOM.
+ReactDOM.render(<App />, document.querySelector('.container'));
