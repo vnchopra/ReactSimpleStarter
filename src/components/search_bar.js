cd239c30b76790b13a1d8aa68ac0ebe926833aaf
Varun Chopra <vnchopra@gmail.com>
2018-07-18 19:03:04 -0700

    Added an event handler to search bar.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,26 @@
-import React from 'react';
+//Here { Component } is equivalent to React.Component
+import React, { Component } from 'react';
 
-//This is a functional component
-const SearchBar = () => {
-  return <input/>;
+//This is a class component
+//extends gives you functionality from the react.component
+class SearchBar extends Component {
+  //a function/method that renders the JSX
+  render() {
+    //added event handler to this input element
+    //where onChange is a react defined property found in react docs.
+    return <input onChange={this.onInputChange} />;
+
+    //equivalent to
+    // return <input onChange={event => console.log(event.target.value)};
+
+  }
+
+  //event handler with argument
+  //event obj
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
 }
 
 export default SearchBar;
