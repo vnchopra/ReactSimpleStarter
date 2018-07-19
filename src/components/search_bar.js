3fcf89aa71d532eee6f05bb2e17e97296f110585
Varun Chopra <vnchopra@gmail.com>
2018-07-18 19:44:22 -0700

    Added a controlled form.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,14 +4,38 @@ import React, { Component } from 'react';
 //This is a class component
 //extends gives you functionality from the react.component
 class SearchBar extends Component {
+
+  constructor (props) {
+    super (props);
+    //For State with React
+    //State is a plain js object that exists on any component that is a class based component.
+    //State is a plain js object to record and react to user events.
+    //With an even occuring/state change,
+    //it forces to re-render
+    //forces children to re-render
+    // = is used only here in the constructor, every where else we use .setState
+    this.state = { term: 'Starting value'};
+  }
+
   //a function/method that renders the JSX
   render() {
     //added event handler to this input element
     //where onChange is a react defined property found in react docs.
-    return <input onChange={this.onInputChange} />;
+    // return <input onChange={this.onInputChange} />;
 
     //equivalent to
-    // return <input onChange={event => console.log(event.target.value)};
+    // return <input onChange={event => console.log(event.target.value)} />;
+
+    //Javascript is referenced inside of jsx with the {} braces
+    //adding the state term value here to 'value' makes it a controlled field
+    return (
+      <div>
+        <input
+        value = {this.state.term}
+        onChange={ event => this.setState( {term: event.target.value} ) }
+        />
+      </div>
+    );
 
   }
 
@@ -19,6 +43,7 @@ class SearchBar extends Component {
   //event obj
   onInputChange(event) {
     console.log(event.target.value);
+    // this.setState( {term: event.target.value} );
   }
 
 }
